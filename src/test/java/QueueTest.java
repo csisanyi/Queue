97b61feaa7919ee7a965c2832eb7b117fe52d20a
Jason Yeo <jayyeo21@gmail.com>
2018-05-29 22:54:00 +0200

    Fixed bug in QueueTest

--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -6,44 +6,6 @@ import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
-public abstract class QueueTest implements Queue {
-
-//    public abstract Queue createInstance();
-
-    @Test
-    public void testMyMethod_True() {
-        Object e = new Object();
-
-        assertFalse(add(e));
-    }
-
-    @Test
-    public void testElement() {
-        assertNotNull(element());
-    }
-
-    @Test
-    public void testOffer() {
-        Object e = new Object();
-        assertFalse(offer(e));
-    }
-
-    @Test
-    public void testPeek() {
-        Object e = new Object();
-        assertEquals(e, peek());
-    }
-
-    @Test
-    public void testPoll() {
-        assertNotNull(poll());
-    }
-
-    @Test
-    public void testRemove() {
-        assertNotNull(remove());
-    }
-
-
+public class QueueTest {
 
 }
