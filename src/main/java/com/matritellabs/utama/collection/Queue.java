2eb19b70f98d7165b8989575991ba37cf2305d7c
sanyi <csisanyi@gmail.com>
2018-05-29 16:52:24 +0200

    interface setup with comments

--- src/main/java/com/matritellabs/utama/collection/Queue.java
+++ src/main/java/com/matritellabs/utama/collection/Queue.java
@@ -1,4 +1,29 @@
 package com.matritellabs.utama.collection;
 
-public class Queue {
+    public interface Queue {
+
+
+        /**Inserts the specified element into this queue if it is possible to do so immediately without
+        violating capacity restrictions,returning true upon success
+        and throwing an IllegalStateException if no space is currently available.*/
+        boolean add(Object e);
+
+
+        /**Retrieves, but does not remove, the head of this queue.*/
+        Object element();
+
+
+        /**Inserts the specified element into this queue if it is possible to do so immediately without
+        violating capacity restrictions.*/
+        boolean offer(Object e);
+
+        /**Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.*/
+        Object peek();
+
+        /**Retrieves and removes the head of this queue, or returns null if this queue is empty.*/
+        Object poll();
+
+        /**Retrieves and removes the head of this queue*/
+        Object remove();
+
 }
