1f92006dda715768ece3287556c7125d59e7b8ae
sanyi <csisanyi@gmail.com>
2018-05-30 10:54:38 +0200

    stashed everything

--- src/main/java/com/matritellabs/utama/collection/Queue.java
+++ src/main/java/com/matritellabs/utama/collection/Queue.java
@@ -7,7 +7,8 @@ package com.matritellabs.utama.collection;
         and throwing an IllegalStateException if no space is currently available.*/
         boolean add(Object e);
 
-        /**Retrieves, but does not remove, the head of this queue.*/
+        /**Retrieves, but does not remove, the head of this queue.
+         * Throws: NoSuchElementException - if this queue is empty.*/
         Object element();
 
         /**Inserts the specified element into this queue if it is possible to do so immediately without
