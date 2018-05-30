a7d8e1910a19dea15f625dad703102a2411e3007
Jason Yeo <jayyeo21@gmail.com>
2018-05-30 09:32:03 +0200

    First implementation of QueueTest commit

--- src/main/java/com/matritellabs/utama/collection/Queue.java
+++ src/main/java/com/matritellabs/utama/collection/Queue.java
@@ -2,16 +2,13 @@ package com.matritellabs.utama.collection;
 
     public interface Queue {
 
-
         /**Inserts the specified element into this queue if it is possible to do so immediately without
         violating capacity restrictions,returning true upon success
         and throwing an IllegalStateException if no space is currently available.*/
         boolean add(Object e);
 
-
         /**Retrieves, but does not remove, the head of this queue.*/
         Object element();
-
 
         /**Inserts the specified element into this queue if it is possible to do so immediately without
         violating capacity restrictions.*/
