4bbe0125fd8f92976f261b18d331d5adf4152acf
sanyi <csisanyi@gmail.com>
2018-05-30 11:46:35 +0200

    logging implemented

--- src/main/java/com/matritellabs/utama/collection/QueueImplementation.java
+++ src/main/java/com/matritellabs/utama/collection/QueueImplementation.java
@@ -1,5 +1,8 @@
 package com.matritellabs.utama.collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -8,31 +11,43 @@ public class QueueImplementation implements Queue {
 
     List<Object> myQueue = new ArrayList<>();
     final int capacity = 50;
+    private static Logger logger = LoggerFactory.getLogger(QueueImplementation.class);
 
 
     public boolean add(Object e) {
-        if (myQueue.size() == capacity) {
-            System.out.println("no more capacity");
-            throw new IllegalStateException();
-         } else {
-            myQueue.add(e);
-            return true;
+//        if (myQueue.size() == capacity) {
+//            System.out.println("no more capacity");
+//            throw new IllegalStateException();
+//         } else {
+//            myQueue.add(e);
+//            return true;
+//        }
+        try {
+//            if (myQueue.size() > capacity) {
+                myQueue.add(e);
+//            }
+                return true;
+        } catch (IllegalStateException iex) {
+            return false;
         }
     }
 
     public Object element() {
         if (myQueue.size() > 0) {
+            logger.info("Current head object is " + myQueue.get(0).toString());
             return myQueue.get(0);
         } else {
+            logger.error("The queue is empty");
             throw new NoSuchElementException();
         }
     }
 
     public boolean offer(Object e) {
         if (myQueue.size() == capacity) {
-            System.out.println("no more capacity");
+            logger.error("no more capacity");
             return false;
         } else {
+            logger.info("Object " + e.toString() + " added to the Queue");
             myQueue.add(e);
             return true;
         }
@@ -40,8 +55,10 @@ public class QueueImplementation implements Queue {
 
     public Object peek() {
         if (myQueue.size() > 0) {
+            logger.info("Current head object is " + myQueue.get(0).toString());
             return myQueue.get(0);
         } else {
+            logger.error("The queue is empty");
             return null;
         }
     }
@@ -50,8 +67,10 @@ public class QueueImplementation implements Queue {
         if (myQueue.size() > 0) {
             Object retrievedObject = myQueue.get(0);
             myQueue.remove(0);
+            logger.info(retrievedObject.toString() + " removed from the queue");
             return retrievedObject;
         } else {
+            logger.error("The queue is empty");
             return null;
         }
     }
@@ -60,8 +79,10 @@ public class QueueImplementation implements Queue {
         if (myQueue.size() > 0) {
             Object retrievedObject = myQueue.get(0);
             myQueue.remove(0);
+            logger.info(retrievedObject.toString() + " removed from the queue");
             return retrievedObject;
         } else {
+            logger.error("The queue is empty");
             throw new NoSuchElementException();
         }
     }
